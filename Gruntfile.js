7c39e91c8354e5df1d40c4cafc52effe4c30697a
cu <u.chris1608@gmail.com>
2017-05-20 14:03:29 +0300

    fixes from @es

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,126 +8,10 @@ module.exports = function(grunt) {
 
     // в папку build
 
-    less: {
-      style: {
-        files: {
-          "build/css/style.css": "less/style.less"
-        }
-      }
-    },
-
-    postcss: {
-      style: {
-        options: {
-          processors: [
-            require("autoprefixer")({browsers: [
-              "last 1 versions",
-              "last 2 Chrome versions",
-              "last 2 Firefox versions",
-              "last 2 Opera versions",
-              "last 2 Edge versions",
-            ]}),
-          ]
-        },
-        src: "build/css/*.css"
-      }
-    },
-
-    csso: {
-      style: {
-        options: {
-          report: "gzip"
-        },
-        files: {
-          "build/css/style.min.css": ["build/css/style.css"]
-        }
-      }
-    },
-
-    imagemin: {
-      images: {
-        options: {
-          optimizationLevel: 3
-        },
-        files: [{
-          expand: true,
-          src: ["build/images/**/*.{png,jpg,gif}"]
-        }]
-      }
-    },
-
-    svgmin: {
-      symbols: {
-        files: [{
-          expand: true,
-          src: ["build/images/icons/*.svg"]
-        }]
-      }
-    },
-
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: [
-            "build/*.html",
-            "build/css/*.css"
-          ]
-        },
-        options: {
-          server: "build",
-          watchTask: true,
-          notify: false,
-          open: true,
-          cors: true,
-          ui: false
-        }
-      }
-    },
-
-    watch: {
-      html: {
-        files: ["*.html"],
-        tasks: ["copy:html"]
-      },
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss", "csso"]
-      }
-    },
-
-    copy: {
-      build: {
-        files: [{
-          expand: true,
-          src: [
-            "fonts/**/*.{woff,woff2}",
-            "images/**/*.{png,jpg,gif,svg}",
-            "js/**/*.js",
-            "*.html"
-          ],
-          dest: "build"
-        }]
-      },
-      html: {
-        files: [{
-          expand: true,
-          src: ["*.html"],
-          dest: "build"
-        }]
-      }
-    },
-
-    clean: {
-      build: ["build"]
-    }
-
-
-    // для тестов
-
     // less: {
     //   style: {
     //     files: {
-    //       "css/style.css": "less/style.less"
+    //       "build/css/style.css": "less/style.less"
     //     }
     //   }
     // },
@@ -145,7 +29,7 @@ module.exports = function(grunt) {
     //         ]}),
     //       ]
     //     },
-    //     src: "css/*.css"
+    //     src: "build/css/*.css"
     //   }
     // },
     //
@@ -155,23 +39,29 @@ module.exports = function(grunt) {
     //       report: "gzip"
     //     },
     //     files: {
-    //       "css/style.min.css": ["css/style.css"]
+    //       "build/css/style.min.css": ["build/css/style.css"]
     //     }
     //   }
     // },
     //
-    // svgstore: {
-    //   symbols: {
+    // imagemin: {
+    //   images: {
     //     options: {
-    //       svg: {
-    //         style: "display: none",
-    //         version: "1.1",
-    //         xmlns: "http://www.w3.org/2000/svg"
-    //       }
+    //       optimizationLevel: 3
     //     },
-    //     files: {
-    //       "images/symbols.svg": ["images/icons/*.svg"]
-    //     }
+    //     files: [{
+    //       expand: true,
+    //       src: ["build/images/**/*.{png,jpg,gif}"]
+    //     }]
+    //   }
+    // },
+    //
+    // svgmin: {
+    //   symbols: {
+    //     files: [{
+    //       expand: true,
+    //       src: ["build/images/icons/*.svg"]
+    //     }]
     //   }
     // },
     //
@@ -179,12 +69,12 @@ module.exports = function(grunt) {
     //   server: {
     //     bsFiles: {
     //       src: [
-    //         "*.html",
-    //         "css/*.css"
+    //         "build/*.html",
+    //         "build/css/*.css"
     //       ]
     //     },
     //     options: {
-    //       server: ".",
+    //       server: "build",
     //       watchTask: true,
     //       notify: false,
     //       open: true,
@@ -195,26 +85,136 @@ module.exports = function(grunt) {
     // },
     //
     // watch: {
+    //   html: {
+    //     files: ["*.html"],
+    //     tasks: ["copy:html"]
+    //   },
     //   style: {
     //     files: ["less/**/*.less"],
     //     tasks: ["less", "postcss", "csso"]
     //   }
     // },
+    //
+    // copy: {
+    //   build: {
+    //     files: [{
+    //       expand: true,
+    //       src: [
+    //         "fonts/**/*.{woff,woff2}",
+    //         "images/**/*.{png,jpg,gif,svg}",
+    //         "js/**/*.js",
+    //         "*.html"
+    //       ],
+    //       dest: "build"
+    //     }]
+    //   },
+    //   html: {
+    //     files: [{
+    //       expand: true,
+    //       src: ["*.html"],
+    //       dest: "build"
+    //     }]
+    //   }
+    // },
+    //
+    // clean: {
+    //   build: ["build"]
+    // }
+
+
+    // для тестов
+
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 1 versions",
+              "last 2 Chrome versions",
+              "last 2 Firefox versions",
+              "last 2 Opera versions",
+              "last 2 Edge versions",
+            ]}),
+          ]
+        },
+        src: "css/*.css"
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "css/style.min.css": ["css/style.css"]
+        }
+      }
+    },
+
+    svgstore: {
+      symbols: {
+        options: {
+          svg: {
+            style: "display: none",
+            version: "1.1",
+            xmlns: "http://www.w3.org/2000/svg"
+          }
+        },
+        files: {
+          "images/symbols.svg": ["images/icons/*.svg"]
+        }
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss", "csso"]
+      }
+    },
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
   grunt.registerTask("build", [
     // для билда
-    "clean",
-    "copy",
-    "less",
-    "postcss",
-    "csso",
-    "imagemin"
-
-    //для тестов
+    // "clean",
+    // "copy",
     // "less",
     // "postcss",
+    // "csso",
+    // "imagemin"
+
+    //для тестов
+    "less",
+    "postcss",
     // "csso",
   ])
 };
